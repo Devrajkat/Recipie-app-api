278cc789a93f68f21dfebf69b120f3bd78a495d5
Devraj <debarajkatuwal@gmail.com>
2022-06-24 23:44:15 -0400

    added settings for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,12 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """ Add x and y and return result."""
     return x + y
+
+
+def subtract(a, b):
+    """ Subtract a and b and return result. """
+    return a-b
