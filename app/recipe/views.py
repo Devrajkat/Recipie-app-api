145ac820357b42d0f852415359954bcd9f2fd537
Devraj <debarajkatuwal@gmail.com>
2022-07-17 11:42:31 -0400

    new ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -45,7 +45,10 @@ class TagViewSet(mixins.UpdateModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IngredientViewSet(mixins.ListModelMixin,
+                        viewsets.GenericViewSet,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin):
     """ Manage ingredient in the database. """
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
